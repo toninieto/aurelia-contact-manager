6891dd077e62c72e99352d49b560cb2eb94e5a23
Toni Nieto <toni@tresipunt.com>
2017-02-25 15:46:53 +0100

    Added all assets files from the tutorial

--- contact-manager/scripts/app-bundle.js
+++ contact-manager/scripts/app-bundle.js
@@ -75,5 +75,141 @@ define('resources/index',["exports"], function (exports) {
   exports.configure = configure;
   function configure(config) {}
 });
+define('utility',["exports"], function (exports) {
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+		value: true
+	});
+	exports.areEqual = areEqual;
+	function areEqual(obj1, obj2) {
+		return Object.keys(obj1).every(function (key) {
+			return obj2.hasOwnProperty(key) && obj1[key] === obj2[key];
+		});
+	};
+});
+define('web-api',['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var latency = 200;
+  var id = 0;
+
+  function getId() {
+    return ++id;
+  }
+
+  var contacts = [{
+    id: getId(),
+    firstName: 'John',
+    lastName: 'Tolkien',
+    email: 'tolkien@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Clive',
+    lastName: 'Lewis',
+    email: 'lewis@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Owen',
+    lastName: 'Barfield',
+    email: 'barfield@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Charles',
+    lastName: 'Williams',
+    email: 'williams@inklings.com',
+    phoneNumber: '867-5309'
+  }, {
+    id: getId(),
+    firstName: 'Roger',
+    lastName: 'Green',
+    email: 'green@inklings.com',
+    phoneNumber: '867-5309'
+  }];
+
+  var WebAPI = exports.WebAPI = function () {
+    function WebAPI() {
+      _classCallCheck(this, WebAPI);
+
+      this.isRequesting = false;
+    }
+
+    WebAPI.prototype.getContactList = function getContactList() {
+      var _this = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var results = contacts.map(function (x) {
+            return {
+              id: x.id,
+              firstName: x.firstName,
+              lastName: x.lastName,
+              email: x.email
+            };
+          });
+          resolve(results);
+          _this.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.getContactDetails = function getContactDetails(id) {
+      var _this2 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var found = contacts.filter(function (x) {
+            return x.id == id;
+          })[0];
+          resolve(JSON.parse(JSON.stringify(found)));
+          _this2.isRequesting = false;
+        }, latency);
+      });
+    };
+
+    WebAPI.prototype.saveContact = function saveContact(contact) {
+      var _this3 = this;
+
+      this.isRequesting = true;
+      return new Promise(function (resolve) {
+        setTimeout(function () {
+          var instance = JSON.parse(JSON.stringify(contact));
+          var found = contacts.filter(function (x) {
+            return x.id == contact.id;
+          })[0];
+
+          if (found) {
+            var index = contacts.indexOf(found);
+            contacts[index] = instance;
+          } else {
+            instance.id = getId();
+            contacts.push(instance);
+          }
+
+          _this3.isRequesting = false;
+          resolve(instance);
+        }, latency);
+      });
+    };
+
+    return WebAPI;
+  }();
+});
 define('text!app.html', ['module'], function(module) { module.exports = "<template><h1>${message}</h1></template>"; });
+define('text!styles.css', ['module'], function(module) { module.exports = "body { padding-top: 70px; }\n\nsection {\n  margin: 0 20px;\n}\n\na:focus {\n  outline: none;\n}\n\n.navbar-nav li.loader {\n    margin: 12px 24px 0 6px;\n}\n\n.no-selection {\n  margin: 20px;\n}\n\n.contact-list {\n  overflow-y: auto;\n  border: 1px solid #ddd;\n  padding: 10px;\n}\n\n.panel {\n  margin: 20px;\n}\n\n.button-bar {\n  right: 0;\n  left: 0;\n  bottom: 0;\n  border-top: 1px solid #ddd;\n  background: white;\n}\n\n.button-bar > button {\n  float: right;\n  margin: 20px;\n}\n\nli.list-group-item {\n  list-style: none;\n}\n\nli.list-group-item > a {\n  text-decoration: none;\n}\n\nli.list-group-item.active > a {\n  color: white;\n}\n"; });
 //# sourceMappingURL=app-bundle.js.map
